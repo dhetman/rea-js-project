2a966a958a1b62183f525fbbc723bef0c75a74a5
Dawid Hetmanczyk <dawid@dhetman.uk>
2017-06-14 22:25:11 +0100

    Project crossed a finish line
    Implemented a number of things:
    - a button to start a new game
    - best time
    - display a random naughty image (easter egg)
    - adjusted default styles so images/shapes won't appear off the screen

--- Reaction-Tester-Project/script.js
+++ Reaction-Tester-Project/script.js
@@ -1,46 +1,98 @@
-var start = new Date ().getTime();
+bestTime = 0;
 
 function getRandomColor(){
-var letters = "0123456789ABCDEF".split('');
-var color = '#';
-for (var i = 0; i < 6; i++) {
-  color += letters[Math.floor(Math.random() * 16)];
-}
-  return color;
+    var letters = "0123456789ABCDEF".split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+
+    return color;
 }
 
 function makeShapeAppear(){
+    var top = Math.random() * 150;
+    var left = Math.random() * 1100;
+    var width = (Math.random() * 200) + 100;
 
-  var top = Math.random() * 400;
-  var left = Math.random() * 400;
-  var width = (Math.random() * 200) + 100;
-  if (Math.random() > 0.5) {
-    document.getElementById("shape").style.borderRadius = "50%";
-  }else{
-    document.getElementById("shape").style.borderRadius = "0";
-  }
-  document.getElementById("shape").style.backgroundColor = getRandomColor();
-  document.getElementById("shape").style.width = width + "px";
-  document.getElementById("shape").style.height = width + "px";
-document.getElementById("shape").style.top = top + "px";
-document.getElementById("shape").style.left = left + "px";
-document.getElementById("shape").style.display = "block";
- var start = new Date ().getTime();
+    if (Math.random() > 0.7 && Math.random() < 0.9) {
+        document.getElementById("naughty").style.display = "block";
+    }else {
+        if (Math.random() > 0.9) {
+            document.getElementById("shape").style.borderRadius = "50%";
+        } else {
+            document.getElementById("shape").style.borderRadius = "0";
+        }
+
+        document.getElementById("shape").style.backgroundColor = getRandomColor();
+        document.getElementById("shape").style.width = width + "px";
+        document.getElementById("shape").style.height = width + "px";
+        document.getElementById("shape").style.top = top + "px";
+        document.getElementById("shape").style.left = left + "px";
+        document.getElementById("shape").style.display = "block";
+    }
 }
+
 function appearAfterDelay(){
-  setTimeout(makeShapeAppear, Math.random() * 2000);
+    setTimeout(makeShapeAppear, Math.random() * 500);
 }
-appearAfterDelay();
+
+function resetTime(){
+    bestTime = 0;
+    document.getElementById("timeTaken").innerHTML = '0s';
+    document.getElementById("timeBest").innerHTML = '0s';
+}
+
+function startGame(){
+    appearAfterDelay();
+    window.start = new Date().getTime();
+}
 
 document.getElementById("shape").onclick = function(){
-  document.getElementById("shape").style.display = "none";
-  var end = new Date().getTime();
-  var timeTaken = (end - start) / 10000;;
-document.getElementById("timeTaken").innerHTML = timeTaken + "s";
-var timeBest = (end - start) / 10000;
-document.getElementById("timeBest").innerHTML = timeBest + "s";
+    document.getElementById("shape").style.display = "none";
 
+    var end = new Date().getTime();
 
+    var timeTaken = (end - window.start) / 1000;
 
-makeAfterDelay();
+    document.getElementById("timeTaken").innerHTML = timeTaken + "s";
+
+    if (bestTime == 0) {
+        bestTime = timeTaken;
+    }
+
+    if (timeTaken >= bestTime) {
+        document.getElementById("timeBest").innerHTML = bestTime + "s";
+    } else {
+        bestTime = timeTaken;
+        document.getElementById("timeBest").innerHTML = bestTime + "s";
+    }
+
+    //window.timeBest = (end - start) / 1000;
+    //document.getElementById("timeBest").innerHTML = timeBest + "s";
+
+}
+document.getElementById("naughty").onclick = function(){
+    document.getElementById("naughty").style.display = "none";
+
+    var end = new Date().getTime();
+
+    var timeTaken = (end - window.start) / 1000;
+
+    document.getElementById("timeTaken").innerHTML = timeTaken + "s";
+
+    if (bestTime == 0) {
+        bestTime = timeTaken;
+    }
+
+    if (timeTaken >= bestTime) {
+        document.getElementById("timeBest").innerHTML = bestTime + "s";
+    } else {
+        bestTime = timeTaken;
+        document.getElementById("timeBest").innerHTML = bestTime + "s";
+    }
+
+    //window.timeBest = (end - start) / 1000;
+    //document.getElementById("timeBest").innerHTML = timeBest + "s";
+
 }
